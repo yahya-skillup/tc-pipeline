3d76e0eaa157d1476e4e5f6cb6324b473e3c44af
yahya-skillup <124680257+yahya-skillup@users.noreply.github.com>
2023-07-08 11:06:31 +0100

    Update steps.sh

--- steps.sh
+++ steps.sh
@@ -2,6 +2,8 @@ kubectl apply -f pvc.yaml
 
 tkn hub install task ibmcloud -n $SN_ICR_NAMESPACE
 
+kubectl create secret docker-registry deploy-icr --docker-server=us.icr.io --docker-username=iamapikey --docker-password=$IBMCLOUD_API_KEY --namespace=$SN_ICR_NAMESPACE
+
 kubectl apply -f serviceaccount.yaml -n $SN_ICR_NAMESPACE
 kubectl apply -f tasks.yaml
 kubectl apply -f pipeline.yaml
